0778625371b08542c3d67c9b4cad646b961a8c03
elle1300 <97467814+elle1300@users.noreply.github.com>
2026-01-31 16:18:37 -0800

    changed to the right encoder

--- src/main/java/frc/BotchoCheese/Constants/SwerveConstants.java
+++ src/main/java/frc/BotchoCheese/Constants/SwerveConstants.java
@@ -178,9 +178,9 @@ public class SwerveConstants {
     private static final Distance kFrontRightYPos = Inches.of(-wheelOffsetFromCenter);
 
     // Back Left
-    private static final int kBackLeftDriveMotorId = 4;
-    private static final int kBackLeftSteerMotorId = 5;
-    private static final int kBackLeftEncoderId = 12;
+    private static final int kBackLeftDriveMotorId = 6;
+    private static final int kBackLeftSteerMotorId = 7;
+    private static final int kBackLeftEncoderId = 13;
     private static final Angle kBackLeftEncoderOffset = Rotations.of(0.427490234375);
     private static final boolean kBackLeftSteerMotorInverted = true;
     private static final boolean kBackLeftEncoderInverted = false;
@@ -189,9 +189,9 @@ public class SwerveConstants {
     private static final Distance kBackLeftYPos = Inches.of(wheelOffsetFromCenter);
 
     // Back Right
-    private static final int kBackRightDriveMotorId = 6;
-    private static final int kBackRightSteerMotorId = 7;
-    private static final int kBackRightEncoderId = 13;
+    private static final int kBackRightDriveMotorId = 4;
+    private static final int kBackRightSteerMotorId = 5;
+    private static final int kBackRightEncoderId = 12;
     private static final Angle kBackRightEncoderOffset = Rotations.of(-0.23608398437);
     private static final boolean kBackRightSteerMotorInverted = true;
     private static final boolean kBackRightEncoderInverted = false;
