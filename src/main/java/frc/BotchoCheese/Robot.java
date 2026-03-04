e9d2025da988c7469830e6d0a87eda44c37ff2ed
elle1300 <97467814+elle1300@users.noreply.github.com>
2026-03-03 16:52:53 -0800

    Added LimelightHomography.java file

--- src/main/java/frc/BotchoCheese/Robot.java
+++ src/main/java/frc/BotchoCheese/Robot.java
@@ -4,13 +4,9 @@
 
 package frc.BotchoCheese;
 
-import com.ctre.phoenix6.Utils;
-
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -18,7 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.BotchoCheese.Constants.RobotMap;
+import frc.BotchoCheese.Commands.LimelightHomography;
 import frc.BotchoCheese.Utils.LimelightHelpers;
 
 
@@ -69,16 +65,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Yaw", RobotContainer.drivetrain.getState().Pose.getRotation().getDegrees());
 
     if (kUseLimelight) {
-      var driveState = RobotContainer.drivetrain.getState();
-      double headingDeg = driveState.Pose.getRotation().getDegrees(); // this is esentually directly from the external IMU since we barely trust vision angle
-      double omegaRps = Units.radiansToRotations(driveState.Speeds.omegaRadiansPerSecond);
-
-      //assuming limelight starts facing red wall (MUST KNOW STARTING ANGLE) TODO
-      LimelightHelpers.SetRobotOrientation(RobotMap.LIMELIGHT_NAME, headingDeg, 0, 0, 0, 0, 0);
-      var llMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(RobotMap.LIMELIGHT_NAME);
-      if (llMeasurement != null && llMeasurement.tagCount > 0 && omegaRps < 2.0) {
-        RobotContainer.drivetrain.addVisionMeasurement(llMeasurement.pose, Utils.fpgaToCurrentTime(llMeasurement.timestampSeconds));
-      }
+      LimelightHomography.update(RobotContainer.drivetrain);
     }
   
   //AdvantageScope simulation
