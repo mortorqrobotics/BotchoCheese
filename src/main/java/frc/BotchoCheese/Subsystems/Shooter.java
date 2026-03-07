7dc2d652cec31068cd3b4ac80e79ca849220d3f7
elle1300 <97467814+elle1300@users.noreply.github.com>
2026-03-07 14:21:55 -0800

    Removed unused imports

--- src/main/java/frc/BotchoCheese/Subsystems/Shooter.java
+++ src/main/java/frc/BotchoCheese/Subsystems/Shooter.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.BotchoCheese.Robot;
 import frc.BotchoCheese.Utils.LimelightHelpers;
 import frc.BotchoCheese.Constants.RobotMap; // Assuming your IDs are here
 
--- src/main/java/frc/BotchoCheese/Subsystems/ShooterInterpolated.java
+++ src/main/java/frc/BotchoCheese/Subsystems/ShooterInterpolated.java
@@ -14,7 +14,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.BotchoCheese.Robot;
 import frc.BotchoCheese.Utils.LimelightHelpers;
 import frc.BotchoCheese.Constants.RobotMap; // Assuming your IDs are here
 
