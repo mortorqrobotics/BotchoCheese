01b8ac80b30faf3618bd7bdf9dee5297f1b387c7
Zac <zsacher@gmail.com>
2026-03-31 20:48:02 -0700

    Add delay before pivot commands to improve execution timing

--- src/main/java/frc/BotchoCheese/RobotContainer.java
+++ src/main/java/frc/BotchoCheese/RobotContainer.java
@@ -212,7 +212,7 @@ JOYSTICK2_CONTROLLER.rightTrigger().toggleOnTrue(
             intake.runIntake(0.75),
             indexer.runIndexer(0.75),
             feeder.runFeeder(0.75),
-            pivot.pivotUpToRotations(6)
+            Commands.waitSeconds(2.0).andThen(pivot.pivotUpToRotations(6))
         )
     )
 );
@@ -226,7 +226,7 @@ JOYSTICK2_CONTROLLER.rightBumper().toggleOnTrue(
             intake.runIntake(0.75),
             indexer.runIndexer(0.75),
             feeder.runFeeder(0.75),
-            pivot.pivotUpToRotations(6)
+            Commands.waitSeconds(2.0).andThen(pivot.pivotUpToRotations(6))
         )
     )
 );
@@ -240,7 +240,7 @@ JOYSTICK2_CONTROLLER.y().toggleOnTrue(
             intake.runIntake(0.75),
             indexer.runIndexer(0.75),
             feeder.runFeeder(0.75),
-            pivot.pivotUpToRotationsOscillatingVoltage(2.0, 0.5, 0.5)
+            Commands.waitSeconds(2.0).andThen(pivot.pivotUpToRotationsOscillatingVoltage(2.0, 0.5, 0.5))
         )
     )
 );
