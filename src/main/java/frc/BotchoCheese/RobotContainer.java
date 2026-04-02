4991772e97117822acd38f51cdd1e65268e21867
Zac <zsacher@gmail.com>
2026-04-01 21:50:30 -0700

    flip

--- src/main/java/frc/BotchoCheese/RobotContainer.java
+++ src/main/java/frc/BotchoCheese/RobotContainer.java
@@ -408,7 +408,6 @@ public class RobotContainer {
         gyro.setYaw(alliancePose.getRotation().getDegrees());
         drivetrain.resetPose(alliancePose);
         drivetrain.resetRotation(alliancePose.getRotation());
-        drivetrain.seedFieldCentric();
         DebugLog.info("Driver homed pose at hub front for alliance: " + (isRedAlliance ? "Red" : "Blue"));
     }
 
